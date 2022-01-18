4bad0afbc699352176020b13c4a81b3e8d193a95
WaiswaDonnie <jumamayungwe78@gmail.com>
2022-01-18 13:44:14 +0300

    orders route

--- authentication/views.py
+++ authentication/views.py
@@ -7,4 +7,5 @@ from rest_framework.response import Response
 class HelloAuthView(generics.GenericAPIView):
  def get(self,request):
   return Response(data={"message":"Hello Auth"},status=status.HTTP_200_OK)
+  
  
