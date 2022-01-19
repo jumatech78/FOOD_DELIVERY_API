8ac3d66b65551e0c0f1cdc6696433918703877bc
CodeGuyUg <jumamayungwe78@gmail.com>
2022-01-19 16:28:17 +0300

    update

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('',views.HelloAuthView.as_view(),name='hello_auth'),
+    path('')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -12,7 +12,17 @@ class HelloAuthView(generics.GenericAPIView):
 
 
 class UserCreateView(generics.GenericApiView):
- 
+
+ serializer_class=serializers.UserCreationSerializer
+
  def post(self,request):
-  
+  data=request.data
+
+  serializer=self.serializer_class(data=data)
+
+  if serializer.is_valid():
+   serializer.save()
+   return Response(data=serializer.data,status=status.HTTP_201_CREATED)
+
+  return Response(data=serializer.errors,status_code=status.HTTP_400_BAD_REQUEST)
  
