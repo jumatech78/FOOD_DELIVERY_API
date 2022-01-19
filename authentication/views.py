1870d1503b59d1db681651d49a2016fe408ca533
CodeGuyUg <jumamayungwe78@gmail.com>
2022-01-19 16:12:11 +0300

    view.py

--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from rest_framework import generics,status
 from rest_framework.response import Response
+from .models import User
+from . import serializers
 
 # Create your views here.
 
 class HelloAuthView(generics.GenericAPIView):
  def get(self,request):
   return Response(data={"message":"Hello Auth"},status=status.HTTP_200_OK)
+
+
+class UserCreateView(generics.GenericApiView):
+ 
+ def post(self,request):
   
  
